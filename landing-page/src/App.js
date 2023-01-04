e621e37b0617ac9535f28cb58622f0b8bdab5852
akinyiliz <lizzieke84@gmail.com>
2023-01-04 07:06:22 +0300

    define layout of the webpages using react routing

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home';
+import Features from './components/features/Features';
+import About from './components/about/About';
+
 function App() {
-	return <div>Hello</div>;
+	return (
+		<div>
+			<Router>
+				<NavBar />
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					></Route>
+					<Route
+						path="/features"
+						element={<Features />}
+					></Route>
+					<Route
+						path="/about"
+						element={<About />}
+					></Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
