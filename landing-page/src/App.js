1fe222dc04d4c4edbaf7f2c417f2702ac8256895
akinyiliz <lizzieke84@gmail.com>
2023-01-05 15:38:46 +0300

    remove react-router-dom ad its routing components

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,5 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
 import Features from './components/features/Features';
@@ -8,23 +6,10 @@ import About from './components/about/About';
 function App() {
 	return (
 		<div>
-			<Router>
-				<NavBar />
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
-					></Route>
-					<Route
-						path="/features"
-						element={<Features />}
-					></Route>
-					<Route
-						path="/about"
-						element={<About />}
-					></Route>
-				</Routes>
-			</Router>
+			<NavBar />
+			<Home />
+			<Features />
+			<About />
 		</div>
 	);
 }
